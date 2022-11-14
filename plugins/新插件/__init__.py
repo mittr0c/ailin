40a3840651d15cd897358c19ca2fd93b652e5bd7
mittr0c <118056789+mittr0c@users.noreply.github.com>
2022-11-14 16:02:05 +0800

    Delete plugins/新插件 directory

--- plugins/新插件/__init__.py
+++
@@ -1,7 +0,0 @@
-from nonebot.permission import SUPERUSER
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
-from nonebot.adapters.onebot.v11 import GROUP_ADMIN, GROUP_OWNER
-from nonebot import on_command
-from nonebot.adapters.onebot.v11 import MessageSegment   #发图片用的
-
-from plugins.fun import *
